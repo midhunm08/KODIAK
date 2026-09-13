2be5515a85a285cf89c7af6c0f86ca023b4dc028
midhunm08 <midhunmd07@gmail.com>
2026-09-13 20:22:41 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://tqvtzdeppaorpcsstgbp.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_rPrji_P-ZmEwKuTh9jQWGQ_vaVWoPKq";
+
+const supabaseClient = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
